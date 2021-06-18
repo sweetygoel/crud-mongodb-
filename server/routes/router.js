ec417e94440dd5a9cf415ea646d7e6ebb5a739f4
sweetygoel <60649407+sweetygoel@users.noreply.github.com>
2021-06-18 05:53:07 +0530

    second commit

---
+++ server/routes/router.js
@@ -0,0 +1,41 @@
+const express=require('express')
+const MongoClient=require('mongodb').MongoClient
+const route=express.Router()
+const connection=require('../database/connection')
+
+//we use route in place of app
+
+//first page
+// route.get('/',(req,res)=>{
+//     res.render('index')
+// });
+
+//addcontact page render
+route.get('/addContact',(req,res)=>{
+    res.render('addContact')
+})
+
+//updatepage render
+// route.get('/updateContact',(req,res)=>{
+//     res.render('updateContact')
+// })
+
+
+//export route so that we can use in server file
+module.exports=route
+
+
+// //first page
+// app.get('/',(req,res)=>{
+//     res.render('index')
+// });
+
+// //addcontact page render
+// app.get('/addContact',(req,res)=>{
+//     res.render('addContact')
+// })
+
+// //updatepage render
+// app.get('/updateContact',(req,res)=>{
+//     res.render('updateContact')
+// })
